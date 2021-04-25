569c8e5c19b7f18b4318f9c98f323b47af603e4c
Robin Snelson <rsnelson@heliostech.co.uk>
2021-04-25 18:56:24 +0100

    Remove files

--- Azure/Manage_Servers_PowerState/automaterunbook.ps1
+++
@@ -1,7 +0,0 @@
-$account = Get-AzAutomationAccount
-
-$Runbook = Get-AzAutomationRunbook -AutomationAccountName $account.AutomationAccountName -ResourceGroupName $account.ResourceGroupName | Where-Object {$_.Name -eq 'Server_Management'}
-
-$Runbook
-
-Set-AzAutomationRunbook -Name $Runbook.AutomationAccountName -ResourceGroupName $account.ResourceGroupName -AutomationAccountName $account -Description "Starts and stops machines on a schedule"
--- Azure/Manage_Servers_PowerState/import-automate.ps1
+++
@@ -1,4 +0,0 @@
-
-
-
-Import-AzAutomationRunbook  -Path "/Users/robin/repos/Powershell/Azure/Manage_Servers_PowerState/test_script.ps1" -AutomationAccountName "azrgsautoacc01" -Name "Sample_Runbook" -ResourceGroupName "rgsazperm" -Type "PowerShell"
